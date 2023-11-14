c6dd9fafda7d33f9ab5214588f439019bdd9e073
Johan Steven Ramirez Murcia <johans.ramirez@udla.edu.co>
2023-11-13 22:13:38 -0500

    Cambios en las vistas Agenda y servicios (aun por completar)

--- Web/Controllers/AgendaController.cs
+++ Web/Controllers/AgendaController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Data;
 
 namespace Web.Controllers
 {
     public class AgendaController : Controller
     {
+        Procedimientos cn= new Procedimientos();
         public IActionResult Index()
         {
+            ViewBag.usua=cn.obtenerUsuarios();
             return View();
         }
     }
