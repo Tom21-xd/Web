473856f5a3dc5376f327fefff6a6988afd19dd86
Johan Steven Ramirez Murcia <johans.ramirez@udla.edu.co>
2023-11-15 20:45:22 -0500

    Gestionar servicio funcional (revisar por si hay algun error)

---
+++ Web/Models/ServicioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Models
+{
+    public class ServicioModel
+    {
+        public int Id { get; set; }
+        public Boolean Estado { get; set; }
+        public string? Nombre { get; set; }
+        public float valor {  get; set; }
+        public string? descripcion {  get; set; }
+    }
+}
