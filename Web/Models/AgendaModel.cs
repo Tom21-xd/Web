e575477082cc344c08d2935510ed3d86e8e841db
Johan Steven Ramirez Murcia <johans.ramirez@udla.edu.co>
2023-11-20 19:01:49 -0500

    Gestionar agenda casi listo ;D

---
+++ Web/Models/AgendaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Models
+{
+    public class AgendaModel
+    {
+        public int Id { get; set; }
+        public string? Fecha { get; set; }
+        public string? Hora { get; set; }
+        public bool Estado { get; set; }
+        public string? NombreUsua { get; set; }
+    }
+}
