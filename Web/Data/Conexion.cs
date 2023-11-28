12ee601e5b3ee954f77ce200afce5a5efa8ebe9d
Jhoan Sebastian Sierra Perdomo <jhoa.sierra@udla.edu.co>
2023-11-28 09:34:17 -0500

    modificacion mostrar servicios a realizar reserva

--- Web/Data/Conexion.cs
+++ Web/Data/Conexion.cs
@@ -8,8 +8,8 @@ namespace Web.Data{
 
         protected void Conectar(){
             try {
-                connection = new MySqlConnection("Server=web-bd.mysql.database.azure.com;UserID = tom;Password=Johan.Sebastian;Database=bd_web;SslMode=Required;SslCa={path_to_CA_cert};");
-                //connection = new MySqlConnection("Server=localhost;Port=3306;Database=bd_web;Uid=root;Pwd=0518");
+                //connection = new MySqlConnection("Server=web-bd.mysql.database.azure.com;UserID = tom;Password=Johan.Sebastian;Database=bd_web;SslMode=Required;SslCa={path_to_CA_cert};");
+                connection = new MySqlConnection("Server=localhost;Port=3306;Database=bd_web;Uid=root;Pwd=2002");
                 connection.Open();
             }catch(Exception e) {
                 Console.WriteLine(e.ToString());
