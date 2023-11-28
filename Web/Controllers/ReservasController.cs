293c7367cb0e124e622108241f2eb3af2d9b564b
Jhoan Sebastian Sierra Perdomo <jhoa.sierra@udla.edu.co>
2023-11-28 02:37:35 -0500

    Vista Realizar Reserva Funcional (en teoria)

--- Web/Controllers/ReservasController.cs
+++ Web/Controllers/ReservasController.cs
@@ -18,10 +18,47 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult crearReservaServicio()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string nombreUsuario = User.Identity.Name;
+                ViewBag.usuario = cn.obtenerUsua(nombreUsuario);
+            }
+            ViewBag.servicios = cn.obtenerservicios();
+            return View();
+        }
+
+        public IActionResult MisReservas()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string nombreUsuario = User.Identity.Name;
+                ViewBag.usuario = cn.obtenerUsua(nombreUsuario);
+            }
+            return View();
+        }
+
         [HttpPost]
         public ActionResult obtenerAgendaFecha(string nombre1, string apellido1, string fecha)
         {
             return Json(cn.obtenerAgendaFecha(nombre1, apellido1, fecha));
+        }
+
+        public ActionResult obtenerFechas(int id_empleado, int id_servicio)
+        {
+            return Json(cn.obtenerFechas(id_empleado, id_servicio));
+        }
+
+        public ActionResult obtenerEmpleadosServicio(int id_servicio)
+        {
+            return Json(cn.obtenerEmpleadosServicio(id_servicio));
+        }
+
+        public void crearReserva(int id_usuario, int id_empleado, string fechaReserva, string horaReserva)
+        {
+            cn.crearReserva(id_usuario, id_empleado, fechaReserva, horaReserva);
+            //return RedirectToAction("MisReservas", "reservas");
         }
     }
 }
