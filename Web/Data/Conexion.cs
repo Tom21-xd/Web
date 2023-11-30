8e3fa4ef1c4a4959e33cf4459480b61423bff187
Johan Steven Ramirez Murcia <johans.ramirez@udla.edu.co>
2023-11-29 19:54:19 -0500

    Eliminar cadena de conexión

--- Web/Data/Conexion.cs
+++ Web/Data/Conexion.cs
@@ -8,8 +8,8 @@ namespace Web.Data{
 
         protected void Conectar(){
             try {
-                connection = new MySqlConnection("Server=web-bd.mysql.database.azure.com;UserID = tom;Password=Johan.Sebastian;Database=bd_web;SslMode=Required;SslCa={path_to_CA_cert};");
-                //connection = new MySqlConnection("Server=localhost;Port=3306;Database=bd_web;Uid=root;Pwd=2002");
+                //connection = new MySqlConnection("Server=nombre_del_host;UserID = {usuario};Password={contraseña};Database={nombre_basededatos};SslMode=Required;SslCa={path_to_CA_cert};");
+                connection = new MySqlConnection("Server=localhost;Port=3306;Database=bd_web;Uid=root;Pwd=2002");
                 connection.Open();
             }catch(Exception e) {
                 Console.WriteLine(e.ToString());
